40c07353cb6e2920a4cd0274e9aa4d5faf77e636
Wenmoux <45694869+Wenmoux@users.noreply.github.com>
2020-10-03 18:05:25 +0800

    Update merge.sh

--- es_source/merge.sh
+++ es_source/merge.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # https://stackoverflow.com/questions/23387256/shell-script-to-join-5-or-more-json-files-together
-mkdir ./es_source
+
 shopt -s nullglob
+mkdir /es_source
 declare -a jsons
 jsons=(*.json) # ${jsons[@]} now contains the list of files to concatenate
 echo '[' > esou.json
