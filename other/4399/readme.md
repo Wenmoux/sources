5abf33b03d081855b75af5100a58842b8a9a517e
wenmoux <wenmoux@gmail.com>
2021-09-19 08:55:02 +0800

    增加新活动

--- other/4399/readme.md
+++ other/4399/readme.md
@@ -1,1 +1,4 @@
-闯三关赢大奖 [原](http://m.img4399.com/rds/maintain/game/candyDart//static/js/0.c9812750c499d1b7cfb1.js) [改](https://raw.githubusercontent.com/Wenmoux/sources/master/other/4399/c9812750c499d1b7cfb1.js)
+闯三关赢大奖 [原](http://m.img4399.com/rds/maintain/game/candyDart//static/js/0.c9812750c499d1b7cfb1.js) [改](https://raw.githubusercontent.com/Wenmoux/sources/master/other/4399/candyDart.js)  
+动物大暴走 [原](http://h5.img4399.com/rds/minigame/game/lastBean/js/bundle-89f2718e1e.js) [改](https://raw.githubusercontent.com/Wenmoux/sources/master/other/4399/lastbean.js)  
+玩个锤子 [原](http://m.img4399.com/rds/minigame/game/playHammer//static/js/index.d99c60eaa24a966e4d80.js) [改](https://raw.githubusercontent.com/Wenmoux/sources/master/other/4399/playHammer.js)  
+海上争霸 [原](http://dlstest.img4399.com/redirect/mm.img4399.com/ot/minigame/game/KingOfTheSea/src/project.4a3ae.js) [改](https://raw.githubusercontent.com/Wenmoux/sources/master/other/4399/KingOfTheSea.js)  
--- other/activities.js
+++ other/activities.js
@@ -19,97 +19,9 @@ async function jhy(id) {
     }
     return prize
 }
-async function wzcj() {
-    for (id of [1, 2, 3, 4, 5]) {
-        console.log(`礼包${id}抽奖中...`)
-        let wzcjres = await get("wzry2021", `ChouJiang&tag=${id}`, true)
-        if (wzcjres.key == "no_band") {
-            console.log("您还没有绑定qq")
-        } else if (wzcjres.key == "3001") {
-            console.log(`您的探索值还不够哦~ ${wzcjres.score}/${wzcjres.needMinScore}`)
-            return;
-        } else if (wzcjres.key == "ok") {
-            result += "王者抽奖："+wzcjres.title+"\n"
-            console.log(`恭喜您获得 ${wzcjres.title}`)
-        } else if (wzcjres.key == "3002") {
-            console.log("您已经领过奖啦~")
-        }
-    }
-}
+
 // 
 
-//王者荣耀快乐一夏 07.23 -08.25
-async function wzry() {
-    console.log("\n--------王者荣耀快乐一夏活动开始--------n")
-    let wzryres = await get("wzry2021", "login", true)
-    if (wzryres.config.userDuihuanCode5 == 1) return;
-    if (wzryres.config.day_share != 2) await get("wzry2021", "dayshare")
-    await get("wzry2021", "DayLingShare")
-    await get("wzry2021", "Zhuli&invitecode=crcf804hkfp")
-    await get("wzry2021", "DayLingInvite")
-    if (wzryres.config.day_play_ling != 2) await get("wzry2021", "DailyGamePlay")
-    await get("wzry2021", "DayLingGamedown")
-    wzres = await get("wzry2021", "login", true)
-    if (wzres.config.userDuihuanCode5 == 0) await wzcj()
-    wzryinfo = "王者荣耀：" + wzres.config.score + " 探索值"
-    console.log(wzryinfo)
-    result += wzryinfo
-}
-
-
-//原神集卡活动 7.21-8.21
-async function summer() {
-    console.log("\n--------原神集卡活动开始--------\n")
-    aid = "2021summer/m"
-    let coderes = await axios.get("http://1oner.cn:1919/hykb/all")
-    if (coderes) codeList = coderes.data.message
-    else codeList = ["4cae9d15aa53c"]
-    let needhelp = true
-    while (needhelp) {
-        if (codeList) code = codeList[Math.round(Math.random() * (codeList.length))].yscode
-        else code = codeList[0]
-        console.log(`为${code}助力...`)
-        let helpres = await get(aid, `giftcode&shareCode=${code}`)
-        if (helpres.key == "ok" || helpres.key == "3007") needhelp = false
-    }
-    await get(aid, "playgame")
-    await get(aid, "lingqushiwan")
-    await get(aid, "lingquinvite")
-    await get(aid, "xuyuan&resure=1")
-    await get(aid, "share")
-    await get(aid, "lingqushare")
-    await get(aid, "qiandao")
-    // await get(aid,"GetFuliMa&ma=no_miling")
-    for (a = 0; a < 4; a++) {
-        await get(aid, "jumprw&rwid=" + a)
-        let cdata = await get(aid, "lingqujumprw&rwid=" + a, true)
-        if (cdata.is_huizhang == 1) {
-            console.log(`恭喜您获得卡牌 ${cdata.hz_name}`)
-        } else if (cdata.is_huizhang == 0) {
-            console.log(`恭喜您获得绯樱碎片 ${cdata.wzsp_num} 共${cdata.all_wzsp}`)
-        } else {
-            console.log(cdata.msg)
-        }
-    }
-    let loginres = await get(aid, "login", true)
-    if (loginres) {
-        config = loginres.config
-        mycode = config.invite_code
-        console.log(`${loginres.name}的助力码 ${mycode}\n开始提交助力码`)
-        let res1 = await axios.get(`http://1oner.cn:1919/hykb/info?uid=${loginres.uid}`)
-        if (res1.data.err_code == 0) {
-            let resi = await axios.post("http://1oner.cn:1919/hykb/update", `uid=${loginres.uid}&yscode=${mycode}&nickname=${encodeURI(loginres.name)}`)
-            console.log(resi.data)
-        } else {
-            let resi = await axios.post("http://1oner.cn:1919/hykb/add", `uid=${loginres.uid}&yscode=${mycode}&nickname=${encodeURI(loginres.name)}`)
-            console.log(resi.data)
-        }
-        info = `原神集卡： 绯樱碎片${config.wzsp_nums} 神${config.cid1}泡${config.cid2}鸣${config.cid3}动${config.cid4}不${config.cid5}灭${config.cid6}影${config.cid7}断${config.cid8}  \n`
-        result += info
-        console.log(info)
-    }
-    console.log("\n--------原神集卡活动运行结束--------\n")
-}
 
 //获取任务id
 async function lottery(a, c, b) {
@@ -160,40 +72,32 @@ async function ddd(id) {
     await get("yuyue2020/m", `mycode&comm_id=${id}&isyuyue=0&testkey=4399NoneDeviceId`)
 }
 
-//史莱姆 2021-07-16 ~ 2021-07-31
-async function slm() {
-   console.log("活动已结束")  
-/*    console.log("\n--------夏日福利史莱姆养成计划开始(搜索2021666)--------\n")
-    aid = "2021slm/m"
-    slmdata = await get(aid, "login", true)
-    await Promise.all([
-        get(aid, "gofuli&resure=1"),
-        get(aid, "share"),
-        get(aid, "xinshou&resure=1"),
-        get(aid, "gozhongcao&resure=1")
-    ]);
-    if (slmdata.config.day_guang != 2) {
-        await get(aid, "guangczzl")
-        await get(aid, "guang&resure=1")
-    }
-    let res = await axios.get(
-        "https://huodong3.3839.com/n/hykb/2021slm/m/index.php"
-    );
-    str = res.data.match(/prize1_lingqu_(\d+)/g);
-    for (id of str) {
-        await get(aid, "playgame&gameid=" + id.split("_")[2])
-    }
-    for (id of str) {
-        await get(aid, "lingqushiwan&gameid=" + id.split("_")[2])
-    }
-    let info = await get(aid, "login")
-    if (info.key == "ok") {
-        msg = `史莱姆：魔法值 ${info.config.tizhong}  露珠 ${info.config.maoqiu} \n`
-        result += msg
-        console.log(msg)
-    }
-    console.log("\n--------夏日福利 史莱姆养成计划结束--------\n")
-    */
+//by 饺子 
+async function cfm(){
+  prize="\n穿越火线: "
+  a= "cfm/m"
+  await get(a, "login&comm_id=1&isyuyue=0")
+  await get(a, "DailyQiandao&comm_id=1&isyuyue=0&id=1")
+  await get(a, "DailyGamePlay&comm_id=1&isyuyue=0&id=2")
+  await get(a, "DailyGameLing&comm_id=1&isyuyue=0&id=2")
+  await get(a, "DailyAppJump&comm_id=1&isyuyue=0&id=3")
+  await get(a, "DailyAppLing&comm_id=1&isyuyue=0&id=3")
+  await get(a, "DailyAppJump&comm_id=1&isyuyue=0&id=4")
+  await get(a, "DailyAppLing&comm_id=1&isyuyue=0&id=4")
+  for (i = 1; i < 3; i++) {
+     await get(a, "DailyInviteJump&comm_id=1&isyuyue=0&id=5")
+     await get(a, "DailyShareback&mode=qq&comm_id=1&isyuyue=0&id=5")
+     await get(a, "DailyInviteLing&comm_id=1&isyuyue=0&id=5")
+ 
+   let re = await get(a, "chouqu&comm_id=1&isyuyue=0")
+   if(re.prizename){
+            prize +=re.prizename
+         }else{
+           prize +="无-" //未中奖
+        }
+   }
+   result += prize
+   console.log(result)
 }
 
 async function task1() {
@@ -211,4 +115,5 @@ async function task1() {
     for (id of ids2.data) {
        await ddd(id)
     }
+    await cfm()
 }
